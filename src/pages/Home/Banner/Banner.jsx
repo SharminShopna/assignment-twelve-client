fb57c104dc0fb2b8adb4cb971a85d8ba79172830
shopna's Macbook <sharminshopna149@gmail.com>
2025-01-14 22:27:36 +0600

    banner section done

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,50 @@
+import img1 from '../../../assets/House1.png';
+import img2 from '../../../assets/House2.png';
+import img3 from '../../../assets/House3.png';
+import img4 from '../../../assets/House4.png';
+const Banner = () => {
+    return (
+        <>
+            <div className="carousel w-full mt-24">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img
+                        src={img1}
+                        className="w-full h-[300px] md:h-[600px]" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                       src={img2}
+                       className="w-full h-[300px] md:h-[600px]" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                       src={img3}
+                       className="w-full h-[300px] md:h-[600px]" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img
+                        src={img4}
+                        className="w-full h-[300px] md:h-[600px]" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Banner;
---
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,131 @@
+import {  NavLink} from "react-router-dom";
+import logo from "../../../assets/house.png";
+// import "../App.css";
+// import { useContext, useState } from "react";
+// import { AuthContext } from "../providers/AuthProvider";
+// import { FaRegCircleUser } from "react-icons/fa6";
+// import { FaSun, FaMoon } from "react-icons/fa";
+import "react-tooltip/dist/react-tooltip.css";
+import { Tooltip } from "react-tooltip";
+
+const NavBar = () => {
+//     const { user, logOut, isDarkMode, setIsDarkMode } = useContext(AuthContext);
+//   const location = useLocation();
+    const links = (
+        <>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+           <li>
+            <a>All Properties</a>
+          </li>
+          <li>
+            <a>Dashboard</a>
+          </li>
+          {/* <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About Us</NavLink>
+          </li>
+          {user ? (
+            <li>
+              <NavLink to="/addEquipment">Add Equipment</NavLink>
+            </li>
+          ) : (
+            ""
+          )}  */}
+          {/* {user ? (
+            <li>
+              <NavLink to="/emailMass">My Equipment List</NavLink>
+            </li>
+          ) : (
+            ""
+          )} */}
+        </>
+      );
+    return (
+        <div className="w-full justify-center flex bg-lime-600">
+            <div className="pt-5 fixed w-full bg-lime-600 top-0 z-50 backdrop-filter backdrop-blur-lg bg-opacity-30 ">
+      <div className="navbar max-w-6xl mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <div className="flex gap-1 text-center items-center justify-center">
+            <img className="w-14 h-14 rounded-full bg-lime-700 p-3" src={logo} alt="" />
+            <h2 className="text-2xl font-semibold hidden md:block">
+              HOUSEBOX
+            </h2>
+           
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+
+        {/* <div className="navbar-end space-x-2">
+          {user && user?.email ? (
+            <div className="text-center space-x-2 image">
+              <img className="w-10 h-10 rounded-full" src={user?.photoURL} alt="" />
+              <p className="name">{user.displayName}</p>
+            </div>
+          ) : (
+            <p className="text-3xl text-blue-900">
+              <FaRegCircleUser />
+            </p>
+          )}
+
+          {user && user?.email ? (
+            <button onClick={logOut} className="btn bg-blue-900 text-white">
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link to="/auth/login" className="btn bg-blue-900 text-white" data-tooltip-id="my-tooltip"
+                data-tooltip-content="Please use your correct email and password">
+                Login
+              </Link>
+              <NavLink to="/auth/register" className="btn bg-blue-900 text-white" data-tooltip-id="my-tooltip"
+                data-tooltip-content="Please Register with your personal information">
+                Register
+              </NavLink>
+            </>
+          )}
+
+          
+          {location.pathname === "/" && (
+            <button onClick={toggleDarkMode} className="btn bg-blue-900 text-white">
+              {isDarkMode ? <FaSun /> : <FaMoon />}
+            </button>
+          )}
+        </div> */}
+      </div>
+    </div>
+    <Tooltip id="my-tooltip" />
+        </div>
+    );
+};
+
+export default NavBar;

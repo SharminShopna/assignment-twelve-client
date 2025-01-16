febe08f06bba14b29fcb96ef273e40161407684a
shopna's Macbook <sharminshopna149@gmail.com>
2025-01-16 17:21:43 +0600

    added dashbord link

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,22 +1,86 @@
+import { AiOutlinePropertySafety } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { GiFlamethrowerSoldier } from "react-icons/gi";
+import { IoMdGitPullRequest } from "react-icons/io";
+import { LuTableProperties } from "react-icons/lu";
 import { NavLink, Outlet } from "react-router-dom";
-
 
 const Dashboard = () => {
     return (
         <>
-        <div>
-            <div className="w-64 min-h-screen bg-lime-700">
-                <ul className="property p-4">
+        <div className="flex">
+            <div className="w-64 min-h-screen bg-lime-700 pt-12">
+                <ul className="property p-4 text-gray-100 space-y-2">
                     <li>
-                        <NavLink></NavLink>
+                        <NavLink 
+                            to='/dashboard/agentProfile' 
+                            className={({ isActive }) => 
+                                `flex font-semibold items-center gap-2 px-4 py-2 rounded ${
+                                    isActive ? 'bg-white text-lime-700' : 'hover:bg-lime-600'
+                                }`
+                            }
+                        >
+                            <CgProfile />
+                            Agent Profile
+                        </NavLink>
                     </li>
-
+                    <li>
+                        <NavLink 
+                            to='/dashboard/addProperty' 
+                            className={({ isActive }) => 
+                                `flex font-semibold items-center gap-2 px-4 py-2 rounded ${
+                                    isActive ? 'bg-white text-lime-700' : 'hover:bg-lime-600'
+                                }`
+                            }
+                        >
+                            <AiOutlinePropertySafety />
+                            Add Property
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='/dashboard/myAgentProperty' 
+                            className={({ isActive }) => 
+                                `flex font-semibold items-center gap-2 px-4 py-2 rounded ${
+                                    isActive ? 'bg-white text-lime-700' : 'hover:bg-lime-600'
+                                }`
+                            }
+                        >
+                            <LuTableProperties />
+                            My Added Properties
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='/dashboard/agentSoldProperty' 
+                            className={({ isActive }) => 
+                                `flex font-semibold items-center gap-2 px-4 py-2 rounded ${
+                                    isActive ? 'bg-white text-lime-700' : 'hover:bg-lime-600'
+                                }`
+                            }
+                        >
+                            <GiFlamethrowerSoldier />
+                            My Sold Properties
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to='/dashboard/agentRequestProp' 
+                            className={({ isActive }) => 
+                                `flex font-semibold items-center gap-2 px-4 py-2 rounded ${
+                                    isActive ? 'bg-white text-lime-700' : 'hover:bg-lime-600'
+                                }`
+                            }
+                        >
+                            <IoMdGitPullRequest />
+                            Requested Properties
+                        </NavLink>
+                    </li>
                 </ul>
             </div>
             <div className="flex-1 p-8">
-                <Outlet></Outlet>
+                <Outlet />
             </div>
-            
         </div>
         </>
     );
